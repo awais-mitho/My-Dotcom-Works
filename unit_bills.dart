29969285b6ed2f96dd8332d8b65bbc57b53f3816
Awais Khokhar <your-email@example.com>
2026-09-22 19:28:03 +0500

    Update Dart files

--- unit_bills.dart
+++ unit_bills.dart
@@ -13,25 +13,17 @@ void main() {
   int unitsConsumed = int.parse(input);
 
   double billAmount = 0;
-  double tax = 0;
 
   if (unitsConsumed >= 0 && unitsConsumed <= 200) {
     billAmount = unitsConsumed * 30;
   } else if (unitsConsumed >= 200  && unitsConsumed <= 800) {
-    billAmount = unitsConsumed * 70;
-    tax += 500; 
+    billAmount = unitsConsumed * 70 + 500;
   } else if (unitsConsumed >= 800 && unitsConsumed <= 1500) {
-    billAmount = unitsConsumed * 100;
-    tax += 500; 
+    billAmount = unitsConsumed * 100 + 500;
   } else {
     billAmount = unitsConsumed * 150;
-    tax += 500; 
   }
-
-  double totalBill = billAmount + tax;
 
   print("Units Consumed: $unitsConsumed");
   print("Bill Amount: Rs. $billAmount");
-  print("Tax: Rs. $tax");
-  print("Total Bill with the Tax: Rs. $totalBill");
 } 
